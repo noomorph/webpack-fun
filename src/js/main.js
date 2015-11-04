1da77916bae1f686a74bea2351814c7aeff8137b
Yaroslav Serhieiev <noomorph@gmail.com>
2015-11-04 13:58:33 +0200

    #13: copying files the weird way

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,13 @@ import 'css/normalize.css';
 import 'css/main.css';
 import $ from 'jquery';
 
+import 'apple-touch-icon.png';
+import 'browserconfig.xml';
+import 'crossdomain.xml';
+import 'favicon.ico';
+import 'humans.txt';
+import 'robots.txt';
+import 'tile-wide.png';
+import 'tile.png';
+
 $("html").removeClass("no-js");
